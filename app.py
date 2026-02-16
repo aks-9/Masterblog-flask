025b2a0fb4bf5f24d1b075bfc0ae56f58e51603b
ashish <ashishsinghgermany@gmail.com>
2026-02-17 00:01:22 +0100

    added a add.html template to add a new blog post

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
-#"Add" Route
-#We’re going to make the /add route, which will display a form for creating a new blog post if a GET request is sent,
-# and add a new blog post to our list if a POST request is sent.
+#Create the Form
+# Create the template for add.html file. The template should contain a form that asks for the relevant details from the user, and submits a POST request to the same route - /add.
 
 import json
-from flask import Flask, render_template, request #import request
-
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 def load_posts():
@@ -16,15 +14,15 @@ def index():
     blog_posts = load_posts()
     return render_template('index.html', posts=blog_posts)
 
-# Create the Route and the Function
-@app.route('/add', methods=['GET', 'POST']) #In the @app.route decorator, we’ve added methods=['GET', 'POST']. This tells Flask that this route should respond to both GET and POST requests.
+
+@app.route('/add', methods=['GET', 'POST'])
 def add():
     if request.method == 'POST':
         # We will fill this in the next step
         pass
     return render_template('add.html')
 
-#If a POST request is sent to this route, we want to add a new blog post. If any other type of request is sent (or by default, a GET request), we want to display a form for adding a new blog post. We’ve set up an if statement to handle this.
+
 
 
 
