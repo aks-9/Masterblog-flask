61683fcce4e02403b840ca48f7b4ff2458a491bd
ashish <ashishsinghgermany@gmail.com>
2026-02-16 23:04:58 +0100

    added -a home route, storage for datastructure, index.html and style.css file to render the blog posts

--- app.py
+++ app.py
@@ -1,11 +1,31 @@
-from flask import Flask
+import json
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+def load_posts():
+    with open('storage.json', 'r') as file:
+        return json.load(file)
+
 @app.route('/')
-def home():
-    return 'Hello, World!'
+def index():
+    blog_posts = load_posts()
+    return render_template('index.html', posts=blog_posts)
+
+
+
+
+
+
+
+
+
+
+
 
 
 if __name__ == '__main__':
-    app.run(host= '0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
+
+
+
